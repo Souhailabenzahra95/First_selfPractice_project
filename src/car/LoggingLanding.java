c4188b92052767ce4dfff6403c8dda04d0245fb3
souha <souhailabenzahra95@gmail.com>
2025-04-07 21:29:15 -0400

    master branche updates

---
+++ src/car/LoggingLanding.java
@@ -0,0 +1,7 @@
+//updates
+package car;
+public class LoggingLanding {
+    public static void main(String[] args) {
+        System.out.println("Logging fonctions");
+    }
+}
--- src/car/Toyota.java
+++
@@ -1,7 +0,0 @@
-package car;
-//Updates test 
-public class Toyota{
-  public static void main(String[] args){
-  System.out.println("Toyota car")
-}
-}
